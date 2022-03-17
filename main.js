e6f0c9ff59416a3b4bf2b6aee9d6851b86469358
Achim Sablotny <achimsablotny@632dbcbd-0522-4fe1-bd55-3f22c8ad1990.fritz.box>
2022-03-17 12:05:13 +0100

    installed prettier

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document
   .querySelector("button[data-button-value='a1789']")
-  .addEventListener("click", function () {
-    document.querySelector(".b1789").classList.toggle("show");
+  .addEventListener('click', function () {
+    document.querySelector('.b1789').classList.toggle('show');
   });
 
 document
   .querySelector("button[data-button-value='a1917']")
-  .addEventListener("click", function () {
-    document.querySelector(".b1917").classList.toggle("show");
+  .addEventListener('click', function () {
+    document.querySelector('.b1917').classList.toggle('show');
   });
 
 document
   .querySelector("button[data-button-value='a1953']")
-  .addEventListener("click", function () {
-    document.querySelector(".b1953").classList.toggle("show");
+  .addEventListener('click', function () {
+    document.querySelector('.b1953').classList.toggle('show');
   });
